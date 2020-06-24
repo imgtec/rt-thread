cb6717bce9d652417842077474b762881218dd38
Du Huanpeng <548708880@qq.com>
2020-06-24 17:38:59 +0800

    bsp: ls2k: [skeleton] pwm driver
    add ioctl routine, decode command names.
    
    Signed-off-by: Du Huanpeng <548708880@qq.com>

--- bsp/ls2kdev/drivers/drv_pwm.c
+++ bsp/ls2kdev/drivers/drv_pwm.c
@@ -13,12 +13,31 @@
 #define RT_USING_PWM
 #ifdef RT_USING_PWM
 
+
+const char *cmdname[] = {
+    "PWM_CMD_ENABLE",
+    "PWM_CMD_DISABLE",
+    "PWM_CMD_SET",
+    "PWM_CMD_GET"
+};
+
+
+static rt_err_t loongson_ioctl(struct rt_device_pwm *device, int cmd, void *arg)
+{
+    rt_err_t rc;
+
+    rt_kprintf("command:[%d-%s]\n", cmd, cmdname[cmd - PWM_CMD_ENABLE]);
+
+    rc = RT_EOK;
+    return rc;
+}
+
 struct rt_device_pwm loongson_pwm = {
     .ops = NULL,
 };
 
 struct rt_pwm_ops loongson_ops = {
-    .control = NULL,
+    .control = loongson_ioctl,
 };
 
 int loongson_pwm_init(void)
